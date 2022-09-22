1040bffccf6fe1c962704804414b2468bc5dbf24
KentaroKoizumi <kentaroskoizumi@gmail.com>
2022-09-23 04:19:44 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import { TextField, Typography, Stack, Box } from '@mui/material'
 import Grid from '@mui/material/Unstable_Grid2';
 import { Container } from '@mui/system'
 import React, { useRef, useState } from 'react';
-import { NumericFormat } from 'react-number-format'
+// import { TextField } from 'react-number-format'
 
 function App() {
   const [ad, setAd] = useState(new Date().getFullYear())
+  const [isMeiji, setIsMeiji] = useState(false)
+  const [isTaisho, setIsTaisho] = useState(false)
+  const [isShowa, setIsShowa] = useState(false)
+  const [isHeisei, setIsHeisei] = useState(false)
+  const [isReiwa, setIsReiwa] = useState(true)
 
   const onChangeHandle = (e) => {
     const val = Number(e.target.value)
@@ -29,6 +34,37 @@ function App() {
         setAd(() => val + 2018)
         break
     }
+    if (1867 < val && val <= 1911){
+      setIsMeiji(true)
+      setIsTaisho(false)
+      setIsShowa(false)
+      setIsHeisei(false)
+      setIsReiwa(false)
+    }else if (1911 < val && val <= 1925 ){
+      setIsMeiji(false)
+      setIsTaisho(true)
+      setIsShowa(false)
+      setIsHeisei(false)
+      setIsReiwa(false)
+    }else if (1925 < val && val <= 1988 ){
+      setIsMeiji(false)
+      setIsTaisho(false)
+      setIsShowa(true)
+      setIsHeisei(false)
+      setIsReiwa(false)
+    }else if (1988 < val && val <= 2018 ){
+      setIsMeiji(false)
+      setIsTaisho(false)
+      setIsShowa(false)
+      setIsHeisei(true)
+      setIsReiwa(false)
+    }else if (2018 < val ){
+      setIsMeiji(false)
+      setIsTaisho(false)
+      setIsShowa(false)
+      setIsHeisei(false)
+      setIsReiwa(true)
+    }
   }
 
   return (
@@ -42,50 +78,60 @@ function App() {
       <Grid container spacing={10} sx={{margin: 1}}>
         <Grid xs>
           <Stack direction="column" spacing={2}>
-            <NumericFormat
+            <TextField
                 id="AD"
                 label="西暦"
                 value={ad}
-                customInput={TextField}
+                type="number"
                 onChange={onChangeHandle}
               />
           </Stack>
         </Grid>
         <Grid xs>
           <Stack direction="column" spacing={2}>
-            <NumericFormat
+            <TextField
               id="meiji"
               label="明治"
               value={ad - 1867}
-              customInput={TextField}
+              type="number"
+              focused={isMeiji}
+              color={isMeiji ? "secondary" : ""}
               onChange={onChangeHandle}
             />
-            <NumericFormat
+            <TextField
               id="taisho"
               label="大正"
               value={ad - 1911}
-              customInput={TextField}
+              type="number"
+              focused={isTaisho}
+              color={isTaisho ? "secondary" : ""}
               onChange={onChangeHandle}
             />
-            <NumericFormat
+            <TextField
               id="showa"
               label="昭和"
               value={ad - 1925}
-              customInput={TextField}
+              type="number"
+              focused={isShowa}
+              color={isShowa ? "secondary" : ""}
               onChange={onChangeHandle}
             />
-            <NumericFormat
+            <TextField
               id="heisei"
               label="平成"
               value={ad - 1988}
-              customInput={TextField}
+              type="number"
+              focused={isHeisei}
+              color={isHeisei ? "secondary" : ""}
               onChange={onChangeHandle}
             />
-            <NumericFormat
+            <TextField
               id="reiwa"
               label="令和"
               value={ad - 2018}
-              customInput={TextField}
+              type="number"
+              focused={isReiwa}
+              color={isReiwa ? "secondary" : ""}
               onChange={onChangeHandle}
             />
           </Stack>
